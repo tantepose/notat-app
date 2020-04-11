270cb7365f0c3c1a891790c379950168b182ed71
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2020-04-11 18:07:33 +0200

    countdown saving, styling

--- src/App.js
+++ src/App.js
@@ -1,49 +1,80 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 
 import firebase from "./components/Firestore";
 
 
 class App extends React.Component {
 
+  // setup state
   constructor() {
-    super();
+    super()
 
     this.state = {
      note: "",
-     path: "nice-kjerre"
+     path: "nice-kjerre",
+     timerOn: false,
+     timeLeft: 1000
     } 
+
+  }
+
+  // initiating app
+  componentDidMount () {
+    // check path to deal with the correct note
+    console.log("now on path " + window.location.pathname + " 💡")
+    const path = window.location.pathname.substr(1) // remove the slash
+
+    this.setState({
+      path: path
+    })
   }
   
+  // note updated
   updateInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+
+    //update state with updated text
+    this.setState({ 
+      [e.target.name]: e.target.value 
+    })
+
+    // countdown logic, save note after X seconds without activity
+    this.setState({timeLeft: 1000 })
+
+    if (!this.state.timerOn) { // timer allready running?
+      console.log("Timer starting... ⏳")
+      this.setState({timerOn: true})
+      var myTimer = setInterval(() => {
+        if (this.state.timeLeft <= 0) { // X milliseconds without activity? reset timer, save note
+          this.setState({timeLeft: 1000 })
+          clearInterval(myTimer)
+          this.setState({timerOn: false})
+          console.log("Time's up! ⏰")
+          this.saveNote()
+        } else { // time not up? proceed countdown
+          this.setState({timeLeft: this.state.timeLeft - 100 })
+        }
+      }, 100)
+    }
   }
 
+  // creating or updating note in database
   saveNote = () => {
-    console.log("saving note... 💾")
-
-    const db = firebase.firestore();
+    console.log("Saving note... 💾")
 
     const data = {
       text: this.state.note,
-      path: this.state.path,
       dateCreated: new firebase.firestore.Timestamp.now()
     }
 
-    db.collection("notes").doc(this.state.path).set(data);
-
-    // const userRef = db.collection("notes").add({
-    //   text: this.state.note,
-    //   path: this.state.path,
-    //   dateCreated: new firebase.firestore.Timestamp.now()
-    // });  
+    const db = firebase.firestore()
+    db.collection("notes").doc(this.state.path).set(data)
   };
+
 
   render() {
     return (
-      <div>
+      <div className="container">
         <textarea
             type="text"
             name="note"
@@ -52,11 +83,7 @@ class App extends React.Component {
            
         />
 
-      <button
-        onClick={this.saveNote}
-      >
-        save note
-      </button>
+    <p id="info">{this.state.path}</p>
 
       </div>
     );
--- src/components/Firestore.js
+++ src/components/Firestore.js
@@ -8,10 +8,10 @@ var firebaseConfig = {
     storageBucket: "notat-app.appspot.com",
     messagingSenderId: "257706087352",
     appId: "1:257706087352:web:71080de3e038190cd4b54d"
-  };
+  }
 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
   console.log("database online ✨")
 
   export default firebase;
