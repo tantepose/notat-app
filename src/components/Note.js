750c519881ddeb3868b6add5e90736b6fb2b061d
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2020-04-15 17:11:49 +0200

    starting work at fish list

---
+++ src/components/Note.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Note = (props) => 
+    <div>
+        <p>{props.name}</p>
+    </div>
+
+export default Note
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import firebase from "./Firestore"
 
+import Note from "./Note"
+
 class NoteList extends React.Component {
 
   constructor() {
     super()
 
     this.state = {
+      notes: []
     }
   }
 
 
   componentDidMount () {
+
+    const db = firebase.firestore()
+    let notes = []
+
+    console.log("🔍 searching for last 10 updated documents...")
+
+    db.collection("notes").orderBy("dateUpdated", "desc").limit(10).get().then( (querySnapshot) => {
+      querySnapshot.forEach(function(doc) {
+          notes.push({
+            id: doc.id, 
+            dateUpdated: doc.data().dateUpdated,
+            dateCreated: doc.data().dateCreated,
+            text: doc.data().text
+          })
+      })
+      this.setState({notes: notes})
+    })
   }
 
   render() {
     return (
       <div className="container">
-          <p>lista går her, si</p>
+          <p>last updated fishes 🐟🐠🐡</p>
+            {
+              this.state.notes.map((note) => 
+                <Note 
+                  name={note.id} 
+                  dateCreated={note.dateCreated}
+                  dateUpdated={note.dateUpdated}
+                  text={note.text}
+                /> 
+              )
+            }
       </div>
     );
   }
+
 }
 
 export default NoteList;
